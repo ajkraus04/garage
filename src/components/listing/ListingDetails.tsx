bd19ed806861b5453c7cb6e7c6f7103bd4cc2ea4
ajkraus04 <ajkraus04@gmail.com>
2025-02-24 10:31:56 -0500

    Fix mobile views

--- src/components/listing/ListingDetails.tsx
+++ src/components/listing/ListingDetails.tsx
@@ -43,41 +43,60 @@ interface ListingDetailsProps {
 export const ListingDetails = ({ listing }: ListingDetailsProps) => {
   return (
     <div className="max-w-[90%] mx-auto">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="space-y-8">
-          <ListingGallery images={listing.images} title={listing.title} />
-          <ListingActions
-            onFreightQuote={handleFreightQuote}
-            onWarrantyCalculate={handleWarrantyCalculate}
-            onFinancingQuote={handleFinancingQuote}
-            onGetInvoice={handleGetInvoice}
-          />
-          <ListingVirtualTour onScheduleCall={handleScheduleCall} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 h-full">
+        <div className="hidden lg:flex flex-col h-full">
+          <div className="flex-grow space-y-8">
+            <ListingGallery images={listing.images} title={listing.title} />
+            <ListingActions
+              onFreightQuote={handleFreightQuote}
+              onWarrantyCalculate={handleWarrantyCalculate}
+              onFinancingQuote={handleFinancingQuote}
+              onGetInvoice={handleGetInvoice}
+            />
+            <ListingVirtualTour onScheduleCall={handleScheduleCall} />
+          </div>
         </div>
 
-        <div className="space-y-8">
-          <ListingHeader
-            title={listing.title}
-            price={listing.price}
-            location={listing.location}
-            isVerified={listing.isVerified}
-            listedDate={listing.listedDate}
-            onShare={handleShare}
-            onSave={handleSave}
-            onSubmitOffer={handleSubmitOffer}
-          />
+        <div className="flex flex-col h-full">
+          <div className="flex-grow space-y-8">
+            <ListingHeader
+              title={listing.title}
+              price={listing.price}
+              location={listing.location}
+              isVerified={listing.isVerified}
+              listedDate={listing.listedDate}
+              onShare={handleShare}
+              onSave={handleSave}
+              onSubmitOffer={handleSubmitOffer}
+            />
 
-          <ListingDescription description={listing.description} />
+            {/* Below I added to handle mobile view to order more relevant */}
+            <div className="block lg:hidden">
+              <ListingGallery images={listing.images} title={listing.title} />
+            </div>
 
-          <ListingSpecifications specifications={listing.specifications} />
+            <ListingDescription description={listing.description} />
 
-          <ListingCategories categories={listing.categories} />
+            <ListingSpecifications specifications={listing.specifications} />
 
-          <ListingContact
-            sellerInfo={listing.sellerInfo}
-            onCall={handleCall}
-            onEmail={handleEmail}
-          />
+            <ListingCategories categories={listing.categories} />
+
+            <ListingContact
+              sellerInfo={listing.sellerInfo}
+              onCall={handleCall}
+              onEmail={handleEmail}
+            />
+
+            <div className="block lg:hidden space-y-8">
+              <ListingActions
+                onFreightQuote={handleFreightQuote}
+                onWarrantyCalculate={handleWarrantyCalculate}
+                onFinancingQuote={handleFinancingQuote}
+                onGetInvoice={handleGetInvoice}
+              />
+              <ListingVirtualTour onScheduleCall={handleScheduleCall} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
